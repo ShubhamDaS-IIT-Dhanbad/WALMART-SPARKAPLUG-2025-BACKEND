92204b38584b2a8e31d5d74257639c27dfa60439
Shubham Das <shubhamdasiitcs@gmail.com>
2025-05-15 22:51:36 +0530

    first

--- app/core/config.py
+++ app/core/config.py
@@ -10,10 +10,14 @@ class Settings(BaseSettings):
     # CORS Configuration
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:3000",
+        "http://localhost:5173",
         "http://localhost:8000",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:8000",
-        "https://chat-bot-iit-ism-frontend-dbwo.vercel.app/"
+        
+        "https://chat-bot-iit-ism-frontend-dbwo.vercel.app/",
+        "https://chat-bot-iit-ism-frontend-dbwo.vercel.app",
+        "https://chat-bot-iit-ism-frontend-dbwo.vercel.app/chat"
     ]
     
     # Database Configuration
